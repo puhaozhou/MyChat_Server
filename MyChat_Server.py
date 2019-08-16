4719d3da218e8dcfc5857dd40802f573017f8a39
markphz <puhaozhou@163.com>
2019-08-16 12:22:17 +0800

    多线程

--- MyChat_Server.py
+++ MyChat_Server.py
@@ -13,6 +13,7 @@ import socket
 import socketserver, json
 import subprocess
 import struct
+import threading
 
 connLst = []  # 用户连接表，有几个用户连接，就会有几个websocket对象
 
@@ -28,8 +29,7 @@ class Connector(object):  # 连接对象类
 
 def get_headers(data):
     headers = {}
-    data = str(data,encoding="utf-8")
-    print(data)
+    data = str(data, encoding="utf-8")
     header, body = data.split("\r\n\r\n", 1)
     header_list = header.split("\r\n")
     for i in header_list:
@@ -63,7 +63,6 @@ def get_data(info):
     for i in range(len(decoded)):
         chunk = decoded[i] ^ mask[i % 4]  # 解码方式
         bytes_list.append(chunk)
-    print(bytes_list)
     body = str(bytes_list, encoding='utf-8')
     return body
 
@@ -83,44 +82,56 @@ def send_msg(conn, msg_bytes):
     return True
 
 
-class MyServer(socketserver.BaseRequestHandler):
-    def handle(self):
-        conn = self.request
-        print("got connection from", self.client_address)
+def generate_token(msg):
+    key = msg + '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
+    ser_key = hashlib.sha1(key.encode('utf-8')).digest()
+    return base64.b64encode(ser_key)
+
+
+def handler_accept(sock):
+    while True:
+        conn, addr = sock.accept()
         data = conn.recv(8096)
-        data = get_data(data)
-        print(data)
+        print("got connection from", addr)
+        headers = get_headers(data)
+        # 对请求头中的sec-websocket-key进行加密
         response_tpl = "HTTP/1.1 101 Switching Protocols\r\n" \
                        "Upgrade:websocket\r\n" \
                        "Connection: Upgrade\r\n" \
                        "Sec-WebSocket-Accept: %s\r\n" \
-                       "WebSocket-Location: ws://%s%s\r\n\r\n"
+                       "WebSocket-Location: ws://%s\r\n\r\n"
 
-        magic_string = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
+        # magic_string = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
         headers = get_headers(data)
         # 对请求头中的sec-websocket-key进行加密
-        value = headers['Sec-WebSocket-Key'] + magic_string
-        ac = base64.b64encode(hashlib.sha1(value.encode('utf-8')).digest())
-        response_str = response_tpl % (ac.decode('utf-8'), headers['Host'], headers['url'])
+        value = headers['Sec-WebSocket-Key']
+        # ac = base64.b64encode(hashlib.sha1(value.encode('utf-8')).digest())
+        token = generate_token(value)
+        print(token)
+        print(headers['Host'])
+        response_str = response_tpl % (token.decode('utf-8'), headers['Host'])
         conn.sendall(bytes(response_str, encoding='utf-8'))
+        t = threading.Thread(target=handler_msg, args=(conn,addr))
+        t.start()
 
+
+def handler_msg(conn,addr):
+    with conn as c:
         registered = False  # 未注册
         while True:
-            conn = self.request
-            data = conn.recv(8096)
-            data = get_data(data)
-            print(data)
-            print(registered)
+            data_recv = c.recv(8096)
+            if data_recv[0:1] == b"\x81":
+                data_parse = get_data(data_recv)
+                print(data_parse)
             try:
-                dataobj = json.loads(data.decode('utf-8'))
+                dataobj = json.loads(data_parse.decode('utf-8'))
             except IOError:
-                print("data is null")
+                print(str("data1 is null"))
                 continue
-            # 如果连接客户端发送过来的信息格式是一个列表且注册标识为False时进行用户注册
             if type(dataobj) == list and not registered:
                 account = dataobj[0]
                 password = dataobj[1]
-                conObj = Connector(account, password, self.client_address, self.request)
+                conObj = Connector(account, password, addr, conn)
                 connLst.append(conObj)
                 register = True
                 continue
@@ -131,7 +142,8 @@ class MyServer(socketserver.BaseRequestHandler):
                 for obj in connLst:
                     if dataobj['to'] == obj.account:
                         # obj.conObj.sendall(data)  # 向目标用户发送数据
-                        send_msg(obj.conObj, bytes(data, encoding="utf-8"))
+                        # send_msg(obj.conObj, bytes(data, encoding="utf-8"))
+                        send_msg(obj.conObj, bytes("recv: {}".format(data_parse), encoding="utf-8"))
                         sendok = True
                 if sendok == False:
                     print('no target valid!')
@@ -139,9 +151,18 @@ class MyServer(socketserver.BaseRequestHandler):
                 # conn.sendall('nobody recevied!'.encode('utf-8'))
                 send_msg(obj.conObj, bytes('nobody recevied!', encoding="utf-8"))
                 continue
+            # send_msg(c, bytes("recv: {}".format(data_parse), encoding="utf-8"))
+
+
+def server_socket():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(("127.0.0.1", 8023))
+    sock.listen(5)
+    t = threading.Thread(target=handler_accept(sock))
+    t.start()
 
 
 if __name__ == '__main__':
-    server = socketserver.ThreadingTCPServer(('127.0.0.1', 8023), MyServer)
     print('waiting for connection...')
-    server.serve_forever()
+    server_socket()
